2b164d3fadf510847537176e60125e298c3f721e
Boris Jarczuk <yarchuk.b@gmail.com>
2021-04-04 18:57:47 +0300

    v1

---
+++ src/main/java/domain/PostInfo.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class PostInfo {
+    private String id;
+    private String authorId;
+    private String date;
+    private String text;
+    private String imageUrl;
+    private String viewImageUrl;
+    private boolean isFavorite;
+    private int likesCount;
+    private int commentsCount;
+    private boolean isLiked;
+    private String commentsUrl;
+    private int repostsCount;
+    private String repostUrl;
+    private String authorUrl;
+    private String readPostUrl;
+    private String capturePost;
+    private int views;
+    private String saveBookmarksUrl;
+    private String reportUrl;
+}
